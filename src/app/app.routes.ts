83459a857738103e431059e412a2f5f27e2216a7
Davidfcg20 <david.felipecg20@gmail.com>
2025-02-28 17:45:17 -0500

    New: Created initial distribution

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'welcome-page',
+        title: 'welcome-page',
+        loadComponent: () => import('./pages/welcome-page/welcome-page.component'),
+      },
+];
---
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  imports: [],
+  templateUrl: './welcome-page.component.html',
+})
+export default class WelcomePageComponent { }
