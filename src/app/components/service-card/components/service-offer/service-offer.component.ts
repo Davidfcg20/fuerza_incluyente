e94ed409f77eec26bb1b97e389a1690a5dbdd847
Davidfcg20 <david.felipecg20@gmail.com>
2025-03-12 08:52:48 -0500

    Update: Fixed margin and pading

--- src/app/components/service-card/components/service-offer/service-offer.component.ts
+++ src/app/components/service-card/components/service-offer/service-offer.component.ts
@@ -38,12 +38,17 @@ export class ServiceOfferComponent {
       information: [
         {
           title:
+            'Descripción del proyecto',
+          text: 'Facilitar condiciones para el desarrollo de habilidades adaptativas, actitudes y valores necesarios para el desarrollo personal y social.',
+        },
+        {
+          title:
             'Talleres para la vida independiente de las personas con discapacidad física',
-          text: 'Lograr que las personas con discapacidad física utilicen herramientas prácticas para aumentar su independencia, su autosuficiencia, autoestima y que sean más productivas y participativas, tanto en su entorno inmediato como en la ciudad.',
+          text: 'Lograr que las personas con discapacidad física utilicen herramientas prácticas para aumentar su independencia, su autosuficiencia, autoestima y que sean más productivas y participativas, tanto en su entorno inmediato como en la ciudad. \n Entrenamiento práctico y teórico enfocado hacia la rehabilitación integral, con el fin de facilitar los procesos de rehabilitación de personas con discapacidad.',
         },
         {
           title: 'Servicios',
-          text: '1. Apoyar en la toma de decisiones, siempre respetando la autodeterminación de la persona. <br> 2. Promover una participación plena en la comunidad favoreciendo la igualdad de oportunidades. <br> 3. Facilitar condiciones para el desarrollo de habilidades adaptativas, actitudes y valores necesarios para el desarrollo personal y social.',
+          text: 'Apoyar en la toma de decisiones, siempre respetando la autodeterminación de la persona. \n Promover una participación plena en la comunidad favoreciendo la igualdad de oportunidades.',
         },
       ],
     },
@@ -56,7 +61,102 @@ export class ServiceOfferComponent {
         },
         {
           title: 'Consultoría',
-          text: 'Creación y fortalecimiento de entidades y organizaciones publico privadas. <br> Consultoría en temas de derecho jurídico. <br> Gestión corporativa. <br> Insolvencia. <br> Seguros. <br> Propiedad intelectual. Litigación. <br> Pensiones. <br> Privacidad y seguridad de datos. <br> Bienes raíces.',
+          text: 'Creación y fortalecimiento de entidades y organizaciones publico privadas. \n Consultoría en temas de derecho jurídico. \n Gestión corporativa. \n Insolvencia. \n Seguros. \n Propiedad intelectual. Litigación. \n Pensiones. \n Privacidad y seguridad de datos. \n Bienes raíces.',
+        },
+      ],
+    },
+    {
+      id: 'CONSEJERÍA ENTRE PARES',
+      information: [
+        {
+          title: 'Descripción del servicio',
+          text: 'Mecanismo de apoyo emocional, se brinda a través de diferentes talleres impartidos por personas con discapacidad que darán el acompañamiento necesario en su proceso de fortalecimiento a otra PcD, que esté empezando con su proceso de fortalecimiento emocional.',
+        },
+      ],
+    },
+    {
+      id: 'PARTICIPACIÓN',
+      information: [
+        {
+          title: 'Formación en derechos de las personas con discapacidad',
+          text: 'Formar en derechos y enfoque de género a las personas con discapacidad, para garantizar la participación efectiva en los diferentes espacios de participación y sus entornos. \n General conocimiento en las diferentes normativas nacionales para el desarrollo de capacidad de intervención de las personas con discapacidad en la construcción de procesos sociales en sus territorios.',
+        },
+      ],
+    },
+    {
+      id: 'MOVILIDAD Y ACCESIBILIDAD',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Impulsar la aplicación de las normas de accesibilidad y las políticas públicas relacionadas en la ciudad de Medellín y el área metropolitana. Se presenta como un proceso de acción interinstitucional e interdisciplinar de diferentes ámbitos que busca la equidad de oportunidades, de inclusión social y de respeto a la diversidad humana y el Diseño para todos. \n Beneficios de la accesibilidad para personas mayores y personas con cualquier tipo discapacidad.',
+        },
+        {
+          title:
+            'Reconocimiento de áreas de uso Público para una Mayor Movilidad',
+          text: 'Consiste en la generación de adecuación de entornos accesibles, bajo los parámetros del diseño universal, espacios y escenarios que permitan una movilidad adecuada y segura de la población con discapacidad, personas con movilidad reducida temporal, personas mayores, madres gestantes y con niños en coches.',
+        },
+      ],
+    },
+    {
+      id: 'COMUNICACIÓN ACCESIBLE',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Generar estrategias comunicacionales y acciones que permitan el acceso y la participación de las personas con discapacidad, en los espacios de consulta y toma de decisiones.',
+        },
+      ],
+    },
+    {
+      id: 'FORMACIÓN CIUDADANA',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Construir un modelo de formación ciudadana sobre diseños universales de inclusión y ajustes razonables, dirigido a agentes educativos, docentes, directivos docentes, público y privado, familias, cuidadores, asistentes personales, comunidad y organizaciones sociales. Formación para los profesionales de la salud que atienden a las personas con discapacidad (fisioterapeutas, rehabilitadores...)',
+        },
+      ],
+    },
+    {
+      id: 'CAPACITACIÓN',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Implementar campañas de conciencia ciudadana sobre temas de inclusión, accesibilidad y corresponsabilidad con el estado, familia y sociedad. \n Verificar el cumplimiento de las leyes, normas y políticas, destinadas a proteger y garantizar los derechos de las personas con discapacidad con oportunidades, promoviendo esencialmente su inclusión educativa y su participación efectiva en la sociedad. \n Favorecer la participación de niños y adolescentes con discapacidad, en los espacios del entorno escolar. \n Promover la participación de niños, niñas y adolescentes con discapacidad en la promoción de sus derechos.',
+        },
+      ],
+    },
+    {
+      id: 'GENERACIÓN DE INGRESOS',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Promoción de la Inclusión de las personas con discapacidad, familiares y cuidadores mediante la generación de ingresos y empleabilidad. \n Promoción de la Inclusión de las personas con discapacidad, familiares y cuidadores mediante la generación de ingresos relacionados a emprendimientos.',
+        },
+      ],
+    },
+    {
+      id: 'SALUD DESDE LA EDUCACIÓN',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Desarrollo de programas de capacitación y transformación de imaginarios en relación a las necesidades y derechos en el ámbito de la salud de las personas con discapacidad.',
+        },
+      ],
+    },
+    {
+      id: 'VIVIENDA ACCESIBLE',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Acciones para garantizar que las viviendas otorgadas a personas con discapacidad y que se consideran ‘adaptadas’ respondan efectivamente a las necesidades de la persona y las familias que vivirán en ellas. \n Acciones para garantizar que las viviendas otorgadas a personas con discapacidad y que se consideran ‘adaptadas’ respondan efectivamente a las necesidades de la persona y las familias que vivirán en ellas.',
+        },
+      ],
+    },
+    {
+      id: 'PROGRAMA DE APOYO',
+      information: [
+        {
+          title: 'Descripción del proyecto',
+          text: 'Brindar apoyo a las personas con discapacidad con personal formado en el área de cuidado y derechos, con conocimiento en normativa y derechos de la población con discapacidad de la Ley 1996 de 2019 y su aplicación.',
         },
       ],
     },
